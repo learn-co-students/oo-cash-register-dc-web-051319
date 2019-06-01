e179940ddd8bd5efe60c5da1c0363f93edef610c
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-01 21:53:44 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 class CashRegister
   
-    attr_accessor :total, :discount, :item, :price, :quantity  
+    attr_accessor :total, :discount, :item, :last_total, :price, :quantity
+       @@items=[]
     
       def initialize(discount=1.0)
         @total = 0.0
@@ -14,16 +15,34 @@ class CashRegister
       end 
       
       def add_item(item, price, quantity=1)
+        
+        @last_total = @total
         @total+=price*quantity.to_f
+        @last_item = {"item" => item, "price" => price*quantity.to_f, "quantity" => quantity}
+        count = 0 
+         until count == quantity
+           @@items << item
+           count+=1
+         end
+         count = 0
       end
       
       def apply_discount
-        binding.pry
          if discount!=1.0
            @total = @total - @total*@discount/100
+           
            "After the discount, the total comes to $800."
         else
-         "There is no discount to apply."
+           "There is no discount to apply."
         end
-    end
+      end
+      
+      def items
+        @@items.last(4)
+      end 
+      
+      def void_last_transaction
+        @total = @last_total
+      end 
+    
 end 
